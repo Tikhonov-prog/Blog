b1edf173d070edbf990f674b0dbee5a9b103eaa7
Vasiliy <belkafavorit91@gmail.com>
2023-09-07 13:07:43 +0900

    all test passed

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -215,9 +215,9 @@ class PostDetailView(DetailView):
         """При попытке пользователя, не являющегося автором поста,
         зайти на неопубликованный пост, его перекинет на главную."""
         if self.get_object().author != self.request.user and (
-            self.get_object().is_published is False or
-            self.get_object().category.is_published is False or
-            self.get_object().pub_date > timezone.now()
+            self.get_object().is_published is False 
+            or self.get_object().category.is_published is False
+            or self.get_object().pub_date > timezone.now()
         ):
             raise http.Http404
         return super().dispatch(request, *args, **kwargs)
